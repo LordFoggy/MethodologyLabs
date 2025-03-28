5d950acb3f08a6ea2ce690981d399e615df3ed3a
LordFoggy <lofoggy@gmail.com>
2025-03-28 19:59:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from games.game1 import play_game as play_game_1
-from games.game2 import play_game as play_game_2
+from games.NOK_game import generate_question as generate_question1
+from games.Proggression_game import generate_question as generate_question2
 from engine.game_engine import run_game
 
 if __name__ == "__main__":
@@ -13,11 +13,12 @@ if __name__ == "__main__":
         choice = input("Your choice: ")
 
         if choice == "1":
-            run_game(play_game_1)
+            run_game(generate_question1)
         elif choice == "2":
-            run_game(play_game_2)
+            run_game(generate_question2)
         elif choice == "0":
             print("Goodbye!")
             break
         else:
             print("Invalid choice, please try again.")
+            
