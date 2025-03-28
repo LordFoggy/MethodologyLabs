7bce3cf91330f3c3a04852079bc3d35f60e6e45d
LordFoggy <lofoggy@gmail.com>
2025-03-28 19:57:54 +0300

    Update game_engine.py

--- engine/game_engine.py
+++ engine/game_engine.py
@@ -1,4 +1,22 @@
-def run_game(play_game_fn):
+def run_game(generate_question_fn):
     rounds = 3
     for _ in range(rounds):
-        play_game_fn()
+        question, correct_answer = generate_question_fn()
+
+        # Выводим вопрос
+        print(question)
+
+        # Получаем ответ от пользователя
+        user_answer = input("Your answer: ")
+
+        # Проверка ответа
+        try:
+            user_answer = int(user_answer)
+            if user_answer == correct_answer:
+                print("Correct!")
+            else:
+                print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+                print("Let's try again!")
+        except ValueError:
+            print("Invalid input! Please enter a number.")
+            
